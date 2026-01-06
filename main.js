466dc42b062f48b69c02f1b7aea989945ecedf93
alina_bkl <alinaburlakova06@gmail.com>
2026-01-06 23:06:07 +0200

    test commit

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-console.log('Hello from Node JS');
+// console.log('Hello from Node JS');
+//
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.cwd());
+//
+// const {a, myFunc} = require('./services/test');
+//
+// console.log(a);
+// myFunc();
 
-console.log(__dirname);
-console.log(__filename);
-console.log(process.cwd());
+
+//HTTP
+const http = require('node:http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+})
