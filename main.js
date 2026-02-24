796072a31b812ee8c855c0644840f8f3c0f9de10
alina_bkl <alinaburlakova06@gmail.com>
2026-02-24 22:47:45 +0200

    lesson 1, hw 1

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-console.log('Hello from Node JS');
+const fs = require('node:fs');
+const afs = require('node:fs/promises')
+const path = require('node:path');
+const readLine = require('node:readline/promises');
 
-console.log(__dirname);
-console.log(__filename);
-console.log(process.cwd());
+const start = async () => {
+    const myFilePath = path.join(process.cwd(), 'emails-2.txt');
+    const gmailFilePath = path.join(process.cwd(), 'gmails.txt');
+
+    const fileStream = fs.createReadStream(myFilePath,  'utf8');
+    const rl = readLine.createInterface({input: fileStream});
+
+    try{
+        for await (const line of rl) {
+            const email = line.split('\t').splice(-1)[0];
+            const domainName = email.split('@').splice(-1)[0];
+            if(domainName === 'gmail.com'){
+                await afs.appendFile(gmailFilePath, `${email}\n`);
+            }
+        }
+    } finally {
+        await rl.close();
+    }
+}
+
+start();
